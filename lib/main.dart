4a4eea6edf7da46a3847afb7f071550be0012a14
user9 <user9@kali>
2023-03-21 05:09:24 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_2/layout.dart';
+import 'package:get/route_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Dash",
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: GoogleFonts.mulishTextTheme(
+          Theme.of(context).textTheme
+        ).apply(
+          bodyColor: Colors.black
+        ),
+        pageTransitionsTheme: const PageTransitionsTheme(builders:{
+          TargetPlatform.iOS:FadeUpwardsPageTransitionsBuilder(),
+          TargetPlatform.android:FadeUpwardsPageTransitionsBuilder()
+        } 
+        ),
+        primaryColor:  Colors.blue
+      ),
+      // ignore: prefer_const_constructors
+      home: SiteLayout(),
+    );
+  }
+}
